037291cad39f4979dd2e52a10a1622075d1f4b76
dragon946470 <Dragon946470@gmail.com>
2020-09-26 17:53:44 -0400

    modified App.js to say {Jose Patrone}

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        My name is {Jose Patrone} and I'm going to master React!
+        My name is Jose Patrone and I'm going to master React!
         </p>
         <a
           className="App-link"
